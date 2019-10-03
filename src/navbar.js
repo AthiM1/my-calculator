c7c94a46bdc9cf418b1fe8f7850d519ba8d29534
Athi Mbondwana <mbondwana.a.s@gmail.com>
2019-10-03 15:21:33 +0200

    my calculator

---
+++ src/navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Head extends Component {
+  render() {
+    return (
+        <div class="navbar">
+  <a href="#home" class="active"> This is my Calculator </a>
+  
+
+</div>
+
+    );
+  }
+}
+
+export default Head;
